a88adfcf92e5e4579ecf8d638c3d3ab332148612
Tomofumi Maki <peace_walker821@yahoo.co.jp>
2018-03-26 23:16:46 +0900

    Create repository

---
+++ Blog Poster/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Blog_Poster
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string Message { get; set; } //TextBoxのTextプロパティにBindするプロパティの定義
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this; //MainWindow.xamlで使用するBindingソースを検索する基準を
+                                     //自分自身(MainWindowクラスオブジェクト)にする
+        }
+
+        private void Post_Button_Click(object sender, RoutedEventArgs e)
+        {
+            //確認ウィンドウを開く
+            var con = new confirmation(Message); //Message="本文"
+            con.Show();
+        }
+    }
+}
---
+++ Blog Poster/confirmation.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Blog_Poster
+{
+    /// <summary>
+    /// confirmation.xaml の相互作用ロジック
+    /// </summary>
+    public partial class confirmation : Window
+    {
+        public confirmation(string msg)
+        {
+            InitializeComponent();
+
+            postingBodyText.Text = msg;
+        }
+
+        private void Posting_Button_Click(object sender, RoutedEventArgs e)
+        {
+            //本文を投稿
+
+            //投稿完了の通知
+            MessageBox.Show("Posted!");
+            this.Close();
+        }
+
+        private void Cancel_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
